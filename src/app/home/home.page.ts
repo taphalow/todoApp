9093ba20f50efd1d42f11b39e3cfc875d39da8c7
unknown <lmoustapha2@gmail.com>
2020-06-17 14:50:55 +0000

    Creating header and segment buttons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,6 +8,29 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class HomePage {
 
-  constructor( private afdb: AngularFireDatabase) {}
+  days = [
+    'Lundi',
+    'Mardi',
+    'Mercredi',
+    'Jeudi',
+    'Vendredi',
+    'Samedi',
+    'Dimanche'
+  ];
+  segment: any;
+  currentDate: string;
+  constructor( private afdb: AngularFireDatabase) {
+    const date = new Date();
+    const options = { weekday: 'long' };
+    this.currentDate = date.toLocaleDateString('fr-FR', options);
+    this.currentDate = this.currentDate.charAt(0).toUpperCase() + this.currentDate.slice(1);
+    this.segment = this.currentDate;
+
+    console.log(this.currentDate);
+  }
+
+  segmentChanged(ev: any) {
+    console.log('Segment changed', this.segment);
+  }
 
 }
